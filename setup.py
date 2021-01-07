41aec9a88f486645926ec831caf09ed0b2fac2ff
Lewis Lee <lewis@vultr.guest>
2021-01-07 13:29:18 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "requests"
+    ],
     python_requires='>=3.6',
 )
