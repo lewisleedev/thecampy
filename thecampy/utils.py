d6a2eca0dd35e93ce8aaedb4e3072945491d157c
2minchul <2_minchul@naver.com>
2021-05-09 14:39:12 +0900

    `utils` 에 `get_kr_now` 추가

--- thecampy/utils.py
+++ thecampy/utils.py
@@ -1,4 +1,13 @@
+from datetime import datetime, timedelta, timezone
+
+seoul_timezone = timezone(timedelta(hours=9), 'Asia/Seoul')
+
+
 def request_url(resource):
     baseUrl = 'https://www.thecamp.or.kr'
     r = "{}/{}".format(baseUrl, resource)
     return r
+
+
+def get_kr_now():
+    return datetime.now(tz=timezone.utc).astimezone(tz=seoul_timezone)
