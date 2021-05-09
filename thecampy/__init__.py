425de69c6bb3168d163641f655ee24e64517c671
2minchul <2_minchul@naver.com>
2021-05-09 14:40:39 +0900

    `__init__.py` 에 `image_utils`, `ThecampyImage` 추가

--- thecampy/__init__.py
+++ thecampy/__init__.py
@@ -1,4 +1,8 @@
-from . import utils
+from . import image_utils, utils
 from .client import client
-from .models import Cookie, Soldier, Message
-from .exceptions import ThecampyException, ThecampyValueError, ThecampyReqError
+from .exceptions import ThecampyException, ThecampyReqError, ThecampyValueError
+from .images import ThecampyImage
+from .models import Cookie, Message, Soldier
+
+__all__ = ['utils', 'image_utils', 'client', 'Cookie', 'Soldier', 'Message', 'ThecampyException', 'ThecampyValueError',
+           'ThecampyReqError', 'ThecampyImage']
